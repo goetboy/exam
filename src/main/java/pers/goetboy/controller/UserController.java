4bb761754c8e2d33c64cc51bcc704996b8def8da
goetboy <32829163+goetboy@users.noreply.github.com>
2018-04-20 02:07:15 +0800

    添加用户管理功能

--- src/main/java/pers/goetboy/controller/UserController.java
+++ src/main/java/pers/goetboy/controller/UserController.java
@@ -1,9 +1,21 @@
 package pers.goetboy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pers.goetboy.entity.sys.User;
+import pers.goetboy.services.UserService;
 
-@RestController
+@RestController()
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+    @RequestMapping(value = "/get",method = RequestMethod.GET)
+    public User get(Integer id){
+        return userService.get(id);
 
+    }
 
 }
